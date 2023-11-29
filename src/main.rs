a0aa6f61b2e23140880dbde5ede1062936d6b609
Fernando <fernando.amorim@st-one.io>
2023-11-29 19:01:33 -0300

    add module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use dialoguer::{theme::ColorfulTheme, Select};
-use rusqlite::{Connection, Result, params};
-use std::io;
+use rusqlite::{Connection, Result};
 use std::process;
+mod functionsql;
 
 fn main() -> Result<()> {
     let conn: Connection = Connection::open("../fruit.db")?;
@@ -23,77 +23,15 @@ fn main() -> Result<()> {
         
         // Use a seleção do usuário
         match selection {
-            0 => insert(&conn),
-            1 => delete(&conn),
-            2 => select_all(&conn),
+            0 => functionsql::insert(&conn),
+            1 => functionsql::delete(&conn),
+            2 => functionsql::select_all(&conn),
             3 => {
                 println!("Saindo...");
-                process::exit(0)
+                process::exit(0);
             },
             _ => unreachable!(),
         }
     }
 }
 
-fn select_all(conn: &Connection){
-    process::Command::new("clear").status().unwrap();
-
-    let mut stmt = conn.prepare("SELECT * FROM fruits").unwrap();
-    let fruit_iter = stmt.query_map(params![], |row| {
-        let value: String = row.get(1)?;
-        Ok(value)
-    }).unwrap();
-
-    println!("Frutas cadastradas:");
-    for fruit in fruit_iter {
-        println!("{}",fruit.unwrap().trim());
-    }
-    println!();
-}
-
-fn insert(conn: &Connection) {
-    process::Command::new("clear").status().unwrap();
-
-    println!("Qual fruta quer cadastrar no sistema?");
-    let mut fruit_name: String = String::new();
-    let _ = io::stdin().read_line(&mut fruit_name);
-    fruit_name = fruit_name.trim().to_uppercase();
-
-    let query_result = conn.query_row(
-        "select 1 from fruits where name = ?",
-        [&fruit_name],
-        |row| row.get::<_, i32>(0),
-    );
-
-    if let Ok(..) = query_result {
-        println!("Está fruta ja esta cadastrada",);
-    } else {
-        let _ = conn.execute("insert into fruits (name) values (?)", [&fruit_name]);
-        println!("Fruta: {} cadastrada com sucesso", &fruit_name);
-    };
-
-    println!();
-}
-
-fn delete(conn: &Connection) {
-    process::Command::new("clear").status().unwrap();
-
-    println!("Qual fruta quer deletar do sistema?");
-    let mut fruit_name: String = String::new();
-    let _ = io::stdin().read_line(&mut fruit_name);
-    fruit_name = fruit_name.trim().to_uppercase();
-
-    let query_result = conn.query_row(
-        "select 1 from fruits where name = ?",
-        [&fruit_name],
-        |row| row.get::<_, i32>(0),
-    );
-
-    if let Ok(..) = query_result {
-        let _ = conn.execute("delete from fruits where name = ?", [&fruit_name]);
-        println!("Fruta: {} deletada com sucesso", &fruit_name);
-    } else {
-        println!("Não existe esta fruta no sistema.",);
-    }
-    println!();
-}
